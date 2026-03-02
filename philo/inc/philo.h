46e276d671801df4735965ca87a34f9088bad184
nicolasgsantana <nicolas07san@gmail.com>
2026-03-02 10:38:02 -0300

    feat: include necessary libraries

--- philo/inc/philo.h
+++ philo/inc/philo.h
@@ -6,7 +6,7 @@
 /*   By: nde-sant <nde-sant@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/03 16:14:21 by nde-sant          #+#    #+#             */
-/*   Updated: 2025/12/03 16:38:45 by nde-sant         ###   ########.fr       */
+/*   Updated: 2026/02/24 15:10:12 by nde-sant         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,5 +15,7 @@
 
 # include <pthread.h>
 # include <sys/time.h>
+# include <stdio.h>
+# include <unistd.h>
 
 #endif
