386113f16ee26a8e26cec027adf89fbaf63b6991
nicolasgsantana <nicolas07san@gmail.com>
2026-03-02 10:38:34 -0300

    feat: add ft_mssleep

--- philo/src/main.c
+++ philo/src/main.c
@@ -6,15 +6,25 @@
 /*   By: nde-sant <nde-sant@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/03 16:13:39 by nde-sant          #+#    #+#             */
-/*   Updated: 2025/12/03 16:36:35 by nde-sant         ###   ########.fr       */
+/*   Updated: 2026/02/26 17:00:45 by nde-sant         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
 
+void	ft_mssleep(unsigned int ms)
+{
+	usleep(ms * 1000);
+}
+
 int	main(int argc, char **argv)
 {
-	(void)argc;
-	(void)argv;
+	unsigned int ms;
+
+	ms = 2000;
+	if (argc == 5 || argc == 6)
+		(void)argv;
+	ft_mssleep(ms);
+	printf("slept for %d seconds\n", ms / 1000);
 	return (0);
 }
