21920bfcbe93bb4c3624cb18c5f3d5e93a3b270f
nicolasgsantana <nicolas07san@gmail.com>
2026-03-04 12:35:58 -0300

    feat: add new structs

--- philo/inc/philo.h
+++ philo/inc/philo.h
@@ -6,7 +6,7 @@
 /*   By: nde-sant <nde-sant@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/03 16:14:21 by nde-sant          #+#    #+#             */
-/*   Updated: 2026/03/03 11:49:24 by nde-sant         ###   ########.fr       */
+/*   Updated: 2026/03/04 12:34:48 by nde-sant         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,26 +17,41 @@
 # include <sys/time.h>
 # include <stdio.h>
 # include <unistd.h>
-
-# define ARGC_ERR "Error: Incorrect Usage\nUse:\n\
-./philo [number of philosophers] \
-[time_to_die](ms) [time_to_eat](ms) [time_to_sleep](ms) \
-[number_of_times_each_philosopher_must_eat](optional)\n"
-# define ARGV_ERR "Error: Args must be numeric\n"
+# include "ft_err.h"
 
 typedef struct s_philo
 {
-	int				count;
-	unsigned int	time_die;
-	unsigned int	time_eat;
-	unsigned int	time_sleep;
-	int				times_must_eat;
-}	t_philo;
+	pthread_t		thread;
+	int				id;
+	int				eating;
+	int				meals_eaten;
+	unsigned int	last_meal;
+	unsigned int	time_to_die;
+	unsigned int	time_to_eat;
+	unsigned int	time_to_sleep;
+	unsigned int	start_time;
+	int				num_of_philos;
+	int				num_times_to_eat;
+	int				*dead;
+	pthread_mutex_t	*r_fork;
+	pthread_mutex_t	*l_fork;
+	pthread_mutex_t	*write_lock;
+	pthread_mutex_t	*dead_lock;
+	pthread_mutex_t	*meal_lock;
+}					t_philo;
+
+typedef struct s_program
+{
+	int				dead_flag;
+	pthread_mutex_t	*write_lock;
+	pthread_mutex_t	*dead_lock;
+	pthread_mutex_t	*meal_lock;
+	t_philo			*philos;
+}					t_program;
 
 int				ft_atoi(const char *nptr);
 unsigned int	ft_atoui(const char *nptr);
 void			ft_putstr_fd(char *s, int fd);
 int				ft_isdigit(int c);
-int				check_args(int argc, char **argv);
 
 #endif
