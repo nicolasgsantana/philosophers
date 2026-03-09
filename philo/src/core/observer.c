98d3af24dc69131d5889f8951405826f70e9fa76
nicolasgsantana <nicolas07san@gmail.com>
2026-03-09 15:01:40 -0300

    feat: add meal_check

--- philo/src/core/observer.c
+++ philo/src/core/observer.c
@@ -6,7 +6,7 @@
 /*   By: nde-sant <nde-sant@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/05 15:06:36 by nde-sant          #+#    #+#             */
-/*   Updated: 2026/03/05 21:06:40 by nde-sant         ###   ########.fr       */
+/*   Updated: 2026/03/09 14:51:51 by nde-sant         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -45,10 +45,32 @@ static int	check_death(t_philo *philos, int num_of_philos)
 	return (0);
 }
 
-// static int	check_meals(t_philo *philo, int num_of_philos)
-// {
+static int	has_eaten_enough(t_philo *philo)
+{
+	pthread_mutex_lock(philo->meal_lock);
+	if (philo->meals_eaten >= philo->num_times_to_eat)
+	{
+		
+		pthread_mutex_unlock(philo->meal_lock);
+		return (1);
+	}
+	pthread_mutex_unlock(philo->meal_lock);
+	return (0);
+}
 
-// }
+static int	check_meals(t_philo *philo, int num_of_philos)
+{
+	int	i;
+
+	i = 0;
+	while (i < num_of_philos)
+	{
+		if (!has_eaten_enough(&philo[i]))
+			return (0);
+		i++;
+	}
+	return (1);
+}
 
 void	*watch(void *ptr)
 {
@@ -58,6 +80,8 @@ void	*watch(void *ptr)
 	while (1)
 	{
 		if (check_death(program->philos, program->num_of_philos))
+			break ;
+		if (check_meals(program->philos, program->num_of_philos))
 			break ;
 	}
 	return (NULL);
